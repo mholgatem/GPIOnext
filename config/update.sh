041d52a622f234b6961f9097513bd1dc9d209af3
mholgatem <holgate.mark1@gmail.com>
2017-10-11 12:33:15 -0700

    Create update.sh

---
+++ config/update.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+VERSION="1.0"
+
+#get script path
+SCRIPT=$(readlink -f $0)
+SCRIPTPATH=`dirname $SCRIPT`
+cd $SCRIPTPATH
+
+#if not root user, restart script as root
+if [ "$(whoami)" != "root" ]; then
+	echo "Switching to root user..."
+	sudo bash $SCRIPT $*
+	exit 1
+fi
+
+shopt -s nocasematch
+if ! [[ "$1" == "-noupdate" ]]; then
+    echo "Performing self-update..."
+    git config --global user.email "none@none.com"
+    git config --global user.name "none@none.com"
+    git checkout master
+    git stash
+    git pull
+    git stash pop
+    git config --global --unset user.email
+    git config --global --unset user.name
+    exec /bin/bash update.sh -noupdate
+fi
+shopt -u nocasematch
+
+sudo systemctl stop gpionext
+sudo systemctl daemon-reload
+sudo systemctl start gpionext
+echo 'update complete'
