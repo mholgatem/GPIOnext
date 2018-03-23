51028c8344d697cea3bdab744469132c8bfe209a
mholgatem <holgate.mark1@gmail.com>
2018-03-23 14:44:52 -0700

    Update curses_menu.py
    add all user input options for "enter"

--- cursesmenu/curses_menu.py
+++ cursesmenu/curses_menu.py
@@ -318,7 +318,7 @@ class CursesMenu(object):
 			self.go_down()
 		elif user_input == curses.KEY_UP:
 			self.go_up()
-		elif user_input in (curses.KEY_ENTER, ord("\n")):
+		elif user_input in {curses.KEY_ENTER, 10, 13}:
 			self.select()
 
 		return user_input
